8b3fd430d3177aaad376c9ad920feba187770f3e
Coihue555 <kevlar555@gmail.com>
2022-03-14 15:19:58 -0300

    getters and setters

--- 05-clases/01_clase.dart
+++ 05-clases/01_clase.dart
@@ -4,9 +4,10 @@ void main() {
 
 Persona persona = new Persona();
 persona..nombre = 'Andres'
-       ..edad   = 34
-       ..bio    = 'Hincha de Club Atletico Cipolletti';
+       ..edad   = 34;
+       //.._bio    = 'Hincha de Club Atletico Cipolletti'; //_bio no se puede usar al ser _privado
 
-print(persona);
+persona.bio = 'Cambie el bio';
+print(persona.bio);
 
 }
--- 05-clases/clases/persona.dart
+++ 05-clases/clases/persona.dart
@@ -3,15 +3,21 @@ class Persona {
   //campos o propiedades
   String? nombre;
   int? edad;
-  String? bio;
+  String _bio = 'Propiedad privada';
   //getters and setters
+  String get bio {
+    return _bio.toUpperCase();            //alternativamente, se puede escribir como una funcion de flecha
+  }                                       //String get info => _bio.toUpperCase();
+  
+  set bio( String texto ){                //set bio( String texto ) => _bio = texto;
+    _bio = texto;
+  }
 
   //constructores
 
   //metodos(funciones)
   @override
-  //String toString() => '$nombre - $edad: $bio';
   String toString() {
-    return '$nombre - $edad: $bio';
+    return '$nombre - $edad: $_bio';      //String toString() => '$nombre - $edad: $bio';
   }
 }

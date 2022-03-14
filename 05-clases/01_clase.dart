3da4accd3fdd2ead4daa58e19d98703c2ff7b62a
Coihue555 <kevlar555@gmail.com>
2022-03-14 11:58:38 -0300

    clases

---
+++ 05-clases/01_clase.dart
@@ -0,0 +1,12 @@
+import 'clases/persona.dart';
+
+void main() {
+
+Persona persona = new Persona();
+persona..nombre = 'Andres'
+       ..edad   = 34
+       ..bio    = 'Hincha de Club Atletico Cipolletti';
+
+print(persona);
+
+}
---
+++ 05-clases/clases/persona.dart
@@ -0,0 +1,17 @@
+
+class Persona {
+  //campos o propiedades
+  String? nombre;
+  int? edad;
+  String? bio;
+  //getters and setters
+
+  //constructores
+
+  //metodos(funciones)
+  @override
+  //String toString() => '$nombre - $edad: $bio';
+  String toString() {
+    return '$nombre - $edad: $bio';
+  }
+}

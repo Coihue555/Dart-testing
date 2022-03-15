0ed58b79baa96796cd43da66c2469b5883374256
Coihue555 <kevlar555@gmail.com>
2022-03-15 09:17:52 -0300

    apis

--- paquetes/bin/paquetes.dart
+++ paquetes/bin/paquetes.dart
@@ -1,5 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 import 'package:paquetes/paquetes.dart' as paquetes;
 
+
 void main(List<String> arguments) {
-  print('Hello world: ${paquetes.calculate()}!');
+  final url = Uri.parse('https://reqres.in/api/users?page=2');
+  http.get(url).then( ( http.Response res) {        //regresa un Future
+    //print(res);                                     //aqui solo vemos la instance de Response, aunque los datos ya estan
+    final body = jsonDecode(res.body);              //jsonDecode crea un mapa con el contenido de res.body
+    print(body);
+    print('page: ${ body['page'] }');
+    print('per_page: ${ body['per_page'] }');
+    print('id del 3er item: ${ body['data'][2]['id'] }');
+  
+  });
 }

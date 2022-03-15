e043da40d598f2cb007fe195f52490c989fe18c4
Coihue555 <kevlar555@gmail.com>
2022-03-15 11:27:35 -0300

    final datos Colombia

--- paquetes/bin/paquetes.dart
+++ paquetes/bin/paquetes.dart
@@ -3,5 +3,8 @@ import 'package:paquetes/paquetes.dart' as paquetes;
 
 void main(List<String> arguments) {
   
-  paquetes.getReqRes_service();
+  //paquetes.getReqRes_service();
+  print('='*20);
+  paquetes.getReqRes_Col();
+  print('='*20);
 }
--- paquetes/lib/paquetes.dart
+++ paquetes/lib/paquetes.dart
@@ -1,32 +1,36 @@
-import 'package:paquetes/classes/reqres_response.dart';
 import 'package:http/http.dart' as http;
-
-void getReqRes_service(){
+import 'package:paquetes/classes/paises_response.dart';
+// void getReqRes_service(){
   
-  final url = Uri.parse('https://reqres.in/api/users?page=2');
-  http.get(url).then( ( http.Response res) {
+//   final url = Uri.parse('https://reqres.in/api/users?page=2');
+//   http.get(url).then( ( http.Response res) {
     
-    final resReqRes = reqResResponseFromJson(res.body);
+//     final resReqRes = reqResResponseFromJson(res.body);
 
-    print('Pagina: ${resReqRes.page}');
-    print('Por pagina: ${resReqRes.perPage}');
-    print('Id del 3er elemento: ${resReqRes.data[2].id}');
+//     print('Pagina: ${resReqRes.page}');
+//     print('Por pagina: ${resReqRes.perPage}');
+//     print('Id del 3er elemento: ${resReqRes.data[2].id}');
     
-    });
-}
+//     });
+// }
 
-//TODO tarea
+
 
 void getReqRes_Col(){
   
   final url = Uri.parse('https://restcountries.com/v2/alpha/col');
   http.get(url).then( ( http.Response res) {
     
-    final resReqRes = reqResResponseFromJson(res.body);
+    final colombia = paisFromJson(res.body);
 
-    print('Pagina: ${resReqRes.page}');
-    print('Por pagina: ${resReqRes.perPage}');
-    print('Id del 3er elemento: ${resReqRes.data[2].id}');
+    print('Pais: ${colombia.name}');
+    print('Poblacion: ${colombia.population}');
+    print('Fronteras: ${colombia.borders}');
+    print('Idioma: ${colombia.languages[0].nativeName}');
+    print('Latitud: ${colombia.latlng[0]}');
+    print('Longitud: ${colombia.latlng[1]}');
+    print('Moneda: ${colombia.currencies[0].name}');
+    print('Bandera: ${colombia.flag}');
     
     });
 }
